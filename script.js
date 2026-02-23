3d7984ff8201c5becb96a7e51c844880690b2d62
abhijeet9113 <abhijeetso900@gmail.com>
2026-02-23 15:24:37 +0000

    Update script.js

--- script.js
+++ script.js
@@ -1,22 +1,19 @@
-function goToSurprise() {
-    window.location.href = "surprise.html";
-}
+function goToSurprise(){ window.location.href="surprise.html"; }
 
-/* Confetti Auto */
-if (document.querySelector(".confetti-container")) {
-    const container = document.querySelector(".confetti-container");
+/* POPUP */
+function openPopup(img){ document.getElementById("popup").style.display="flex"; document.getElementById("popupImg").src=img; }
+function closePopup(){ document.getElementById("popup").style.display="none"; }
 
-    for (let i = 0; i < 150; i++) {
-        let confetti = document.createElement("div");
+/* CONFETTI */
+if(document.querySelector(".confetti-container")){
+    const container=document.querySelector(".confetti-container");
+    for(let i=0;i<150;i++){
+        let confetti=document.createElement("div");
         confetti.classList.add("confetti");
-
-        confetti.style.left = Math.random() * 100 + "vw";
-        confetti.style.top = "-10px";
-        confetti.style.backgroundColor =
-            "hsl(" + Math.random() * 360 + ",100%,50%)";
-        confetti.style.animationDuration =
-            (Math.random() * 3 + 2) + "s";
-
+        confetti.style.left=Math.random()*100+"vw";
+        confetti.style.top="-10px";
+        confetti.style.backgroundColor="hsl("+Math.random()*360+",100%,50%)";
+        confetti.style.animationDuration=(Math.random()*3+2)+"s";
         container.appendChild(confetti);
     }
 }
